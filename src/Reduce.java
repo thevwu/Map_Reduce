fa6157949f60df177102cea8a937ed1b85358b41
Brandon S. Kane <me@brandonskane.com>
2013-11-02 20:26:53 -0400

    complete invertedIndex function

--- src/Reduce.java
+++ src/Reduce.java
@@ -5,6 +5,7 @@ public class Reduce extends Thread {
 	private BoundedBuffer buffer;
 	private int num;
 	private HashMap<String, ArrayList<Tuple>> invertedIndex;
+	;
 
  
 	public Reduce(int num) {
@@ -20,16 +21,25 @@ public class Reduce extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 	
 	public void invertedIndex(String fileName, int lineNum, String word){
 		if(invertedIndex.containsValue(word)){
-			invertedIndex.get(word).insert(new Tuple(filename, lineNum)); //needs to be fixed
+			
+			//if the word is already entered, add a new tuple to that word's arraylist
+			invertedIndex.get(word).add(new Tuple(fileName, lineNum));
+
 		}
 		else{
 			// create new ArrayList
 			// insert tuple into arrayList
 			// insert arrayList into invertedIndex
+			ArrayList<Tuple> listOfWords = new ArrayList<Tuple>();
+			Tuple record = new Tuple(fileName, lineNum);
+			listOfWords.add(record);
+			invertedIndex.put(word, listOfWords);
+
 		}
 	}
 	
